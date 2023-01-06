97619e88e73740ccd1ab8cce1598cdc7a0873605
Harrison Boatman <hrry.boatman2@gmail.com>
2023-01-05 18:35:44 -0600

    updated objects of questions with correct choices

--- 02-Challenge/code/code.js
+++ 02-Challenge/code/code.js
@@ -5,7 +5,12 @@ var ans1 = document.querySelector('.ans1')
 var ans2 = document.querySelector('.ans2')
 var ans3 = document.querySelector('.ans3')
 var ans4 = document.querySelector('.ans4')
-
+var wins;
+var losses;
+var scoreboard = {
+    wins: '0',
+    losses: '0',
+}
 var question1;
 var question2;
 var question3;
@@ -16,41 +21,52 @@ var questions = [
     question1 = {
         question: 'Which of the following is not a coding language',
         answers: ['Javascript', 'Google', 'CSS', 'HTML'],
+        correctanswer: 'Google',
        
     },
     question2 = {
         question: 'Calling an id uses the CSS tag "." and not "#"',
-        answers: ['True', 'False'],
+        answers: ['True', 'False', 'Maybe','I have no idea'],
+        correctanswer: 'False',
     },
     question3 = {
         question: 'Visual Studio reads the code from Top to Bottom',
-        answers: ['True', 'False'],
+        answers: ['True', 'False','Maybe','I have no idea'],
+        correctanswer: 'True',
     },
     question4 = {
         question: 'Which of these items does NOT belong?',
         answers: ['String','Variable','Boolean','Salmon'],
+        correctanswer: 'Salmon',
     },
     question5 = {
         question: 'What is the correct notation for positive iteration in for loop?',
         answers: ['++', 'xx','//','^^'],
+        correctanswer:'++',
         
     }
 
 ];
 
-function displayQuestion (){
-   for(i=0; i<questions.length; i++) 
+function displayQuestion (event){
+   for(i=0; i<questions.length; i++) {
    
-    quest.textContent = JSON.stringify(question1.question)
-    ans1.append(question1.answers[0])
-    ans2.append(question1.answers[1])
-    ans3.append(question1.answers[2])
-    ans4.append(question1.answers[3])
+    quest.textContent = JSON.stringify(questions[i].question)
+    ans1.append(questions[i].answers[i])
+    ans2.append(questions[i].answers[i])
+    ans3.append(questions[i].answers[i])
+    ans4.append(questions[i].answers[i])
     
-}
+    ans1.addEventListener('click', function(){
+        losses++
+        window.alert("WRONG")
+        return displayQuestion();
+    })
+    
+}};
 strtBtn.addEventListener('click', displayQuestion)
 
 console.log(question1);
 console.log(question1.answers)
 console.log(questions.length)
-console.log(questions[2])
+console.log(questions[2]);
